a61d1dfa9a8fd56847165b9c04af7caceabe0250
HGuarines <henriqueguarines@gmail.com>
2025-02-14 00:18:04 -0300

    Q6 a Q8

--- Pre_tarefa_uso_python/Q6.py
+++ Pre_tarefa_uso_python/Q6.py
@@ -1,10 +1,13 @@
-"""
-6. Explique o que é a biblioteca cmath e como usar esta biblioteca nos programas.
-"""
+""" 6. Explique o que é a biblioteca cmath e como usar esta biblioteca nos programas. """
+
 import asp
+import cmath
 
 rsp6 = [
-    ('\n6.', ' ')
+    ('\n6.', ' '),
+    ('A biblioteca cmath foi criada para facilitar e otimizar operações com números complexos',
+     '\nPara utiliza-la basta traze-la ao programa com "import cmath" e será possivel trabalhar com números complexos.'),
+    ('Ex: cmath.sqrt(-1) = ', cmath.sqrt(-1))
 ]
 
 asp.gerar_arquivo_texto('pre_tarefa_uso_python_henrique.txt',
