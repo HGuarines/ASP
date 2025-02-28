c7f507755e37d72f436d6ef7fca704e1004e587b
HGuarines <henriqueguarines@gmail.com>
2025-02-27 23:56:29 -0300

    Alteração formatação 1Q

--- Pre_tarefa_aplicacoes_python/Q1.py
+++ Pre_tarefa_aplicacoes_python/Q1.py
@@ -1,6 +1,7 @@
 """1. Exemplo usando a função pot_comp1f(V, I)"""
 
 import asp
+import numpy as np
 
 # Dados de entrada
 V = asp.pol2ret(220, 0)
@@ -8,11 +9,12 @@ I = asp.pol2ret(10, 0)
 
 # Cálculo
 P = asp.pot_comp1f(V, I)
+P_polar = asp.ret2pol(P)
 
 # Resultado
 rsp1 = [('1. ', 'Dados:'),
         ('V = ', '220 ∠ 0° V'),
         ('I = ', '10 ∠ 0° A'),
-        ('Temos que:\nP = ', '2200 ∠ 0° W')]
+        ('Temos que:\nP = ', f'{P_polar[0]:.1f} ∠ {np.degrees(P_polar[1]):.1f}° W')]
 
 asp.gerar_arquivo_texto('Q1.txt', 'APLICAÇÕES PYTHON', rsp1)
